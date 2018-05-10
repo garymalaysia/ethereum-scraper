aee2ba68cfe6c968ad7b52ac69cd27af409a9aec
learning101 <learning101@learning101s-MacBook-Pro.local>
2018-05-10 11:55:30 -0400

    fixed bug

--- getTrax.js
+++ getTrax.js
@@ -1,8 +1,13 @@
+// Author: Gary Tsai
+// Fordham Univeristy
+// Prerequisites: 
+//	 Install Nodejs && Truffle Framework 
+// Usage: 
+// 	$ truffle exec getTrax.js
 
-var b = web3.eth.blockNumber;
+var b = web3.eth.blockNumber; // gather all block number
 
 for (var i = 0; i < b; i++){
-	var block = web3.eth.getBlock(i); 
 	if (web3.eth.getTransactionFromBlock(i) != null) 
 		{ var trx = web3.eth.getTransactionFromBlock(i); 
 			console.log("Input Value: " + web3.toAscii(trx.input) + "\n"); } }
